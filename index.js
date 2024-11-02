cef378a1c800845ae95c2a32f9557c6ee2806160
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2024-11-02 11:07:14 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+var numeroacomprobar = 11;
+
+if (numeroacomprobar == 10){
+    console.log(`su numero es un ${numeroacomprobar}`)
+}else{
+    if(numeroacomprobar > 10){
+        console.log(`el numero ${numeroacomprobar} es mayor a 10`)
+    }else{
+        console.log(`el numero ${numeroacomprobar} es menor a 10`)
+    }
+}
